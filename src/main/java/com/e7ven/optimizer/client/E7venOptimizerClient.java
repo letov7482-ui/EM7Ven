6997010cdfbb5a5d3fe812cef261b5ae687ec826
letov7482-ui <letov7482@gmail.com>
2026-09-11 20:43:28 +0300

    Add E7venOptimizerClient class for client initialization

---
+++ src/main/java/com/e7ven/optimizer/client/E7venOptimizerClient.java
@@ -0,0 +1,11 @@
+package com.e7ven.optimizer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class E7venOptimizerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[E7ven Optimizer] Client initialized.");
+    }
+}
