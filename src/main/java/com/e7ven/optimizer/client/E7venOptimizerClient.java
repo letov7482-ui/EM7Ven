4298c627d6fc216e3ae7a4928ba8246ec3760e6c
letov7482-ui <letov7482@gmail.com>
2026-09-11 20:46:00 +0300

    Expose RefreshRateManager via static method
    Added a static method to access the RefreshRateManager instance.

--- src/main/java/com/e7ven/optimizer/client/E7venOptimizerClient.java
+++ src/main/java/com/e7ven/optimizer/client/E7venOptimizerClient.java
@@ -4,8 +4,27 @@ import net.fabricmc.api.ClientModInitializer;
 
 public final class E7venOptimizerClient implements ClientModInitializer {
 
+    private static final RefreshRateManager REFRESH_RATE_MANAGER =
+            RefreshRateManager.getInstance();
+
     @Override
     public void onInitializeClient() {
         System.out.println("[E7ven Optimizer] Client initialized.");
+
+        REFRESH_RATE_MANAGER.update();
+
+        System.out.println(
+                "[E7ven Optimizer] Display: "
+                        + REFRESH_RATE_MANAGER.getRefreshRateText()
+        );
+
+        System.out.println(
+                "[E7ven Optimizer] Target Frame Time: "
+                        + REFRESH_RATE_MANAGER.getTargetFrameTimeText()
+        );
+    }
+
+    public static RefreshRateManager getRefreshRateManager() {
+        return REFRESH_RATE_MANAGER;
     }
 }
