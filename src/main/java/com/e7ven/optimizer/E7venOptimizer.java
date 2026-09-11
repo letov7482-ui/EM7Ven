b9a6c9fbd0f9c6e3a38d7acba07cf1b3e7373982
letov7482-ui <letov7482@gmail.com>
2026-09-11 20:36:17 +0300

    Implement E7venOptimizer mod initializer

---
+++ src/main/java/com/e7ven/optimizer/E7venOptimizer.java
@@ -0,0 +1,13 @@
+package com.e7ven.optimizer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class E7venOptimizer implements ModInitializer {
+
+    public static final String MOD_ID = "e7venoptimizer";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[E7ven Optimizer] Initializing...");
+    }
+}
